471088d67bd71cf7aae7082fcb277cebe5b173a4
Zemlin Nikita <Darnik142@gmail.com>
2021-11-21 14:11:17 +0300

    Рисование фигур через нажатие на экран реализовано необходимо реализвать работу интерфейса Cloneable

---
+++ lab5/app/src/main/java/com/example/lab5/Shapes/Circle.java
@@ -0,0 +1,29 @@
+package com.example.lab5.Shapes;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.widget.Toast;
+
+public class Circle extends Shape {
+    Context context;
+    @Override
+    public void draw(Paint paint, Canvas canvas, Context context, float[] pos) {
+                this.context = context;
+                paint.setColor(Color.parseColor(setColor(color)));
+                canvas.drawCircle(pos[0],pos[1], 143,  paint);
+                discriptor();
+    }
+
+    @Override
+    public String setColor(String color) {
+        color = "#EADEDB";
+        return color;
+    }
+
+    @Override
+    public void discriptor() {
+        Toast.makeText(context, "Нарисован круг", Toast.LENGTH_LONG).show();
+    }
+
+}
