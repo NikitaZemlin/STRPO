f7acb6c5db6599661153c5307d53019d21dd8be2
Zemlin Nikita <Darnik142@gmail.com>
2021-11-24 19:17:38 +0300

    Изменены названия проектов

---
+++ task_1/app/src/main/java/com/example/lab2/Circle.java
@@ -0,0 +1,35 @@
+package com.example.lab2;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.widget.LinearLayout;
+
+public class Circle extends Shape{
+    float radius;
+    String color;
+    public Circle(String color, float radius) {
+        super(color);
+        this.color = color;
+        this.radius = radius;
+    }
+
+
+    @Override
+    double area() {
+        return 3.14*radius*radius;
+    }
+
+    @Override
+    public void draw(Paint paint, Canvas canvas) {
+                paint.setColor(Color.parseColor(color));
+                canvas.drawCircle(200,100, radius,  paint);
+    }
+
+    @Override
+    public String name() {
+        return "Цвет круга в RGB: " + super.color +  " и площадь: " + area();
+    }
+
+}
